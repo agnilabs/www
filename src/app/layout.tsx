7c64bedc63b00794522dff2269c0f518aaa00315
Aditya Addepalli <adi.addepalli@gmail.com>
2025-01-09 15:59:19 -0600

    feat: integrate Vercel Analytics and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import "~/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
-import { Inter } from "next/font/google";
-
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "~/components/theme-provider";
 import { siteConfig } from "~/config/site";
 
@@ -45,6 +45,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
